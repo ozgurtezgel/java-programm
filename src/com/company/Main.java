b18307a9ff0d16cc9abb71b50d7ca389636fcc17
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-03-22 19:06:52 +0100

    implemented the method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(5);
+        list.add(2);
+        list.add(-1);
+        list.add(2);
+        list.add(-5);
+        list.add(7);
+
+
+        int result = findTheClosestIntegerToZero(list);
+        System.out.println(result);
+
+        list.add(1);
+        int result2 = findTheClosestIntegerToZero(list);
+        System.out.println(result2);
+    }
+
+    public static int findTheClosestIntegerToZero(List<Integer> list) {
+        int closest = Integer.MAX_VALUE;
+
+        for (int number:list) {
+            if (number > 0 && number <= Math.abs(closest)) {
+                closest = number;
+            } else if (number < 0 && -number < Math.abs(closest)){
+                closest = number;
+            } else if (number == 0) {
+                closest = number;
+                break;
+            }
+        }
+        return closest;
     }
 }
+
