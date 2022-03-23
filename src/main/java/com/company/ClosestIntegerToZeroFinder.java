83d78c753e198c66b66c30a8deb29be8e0827578
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-03-23 10:04:39 +0100

    updated the method

--- src/main/java/com/company/ClosestIntegerToZeroFinder.java
+++ src/main/java/com/company/ClosestIntegerToZeroFinder.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public class ClosestIntegerToZeroFinder {
 
-    public int findTheClosestIntegerToZero(List<Integer> list) {
+    /**
+     * The method finds the closest integer to zero. If there is a tie, the positive value is chosen.
+     * If the list ist empty, zero is returned.
+     * @param list list of integers
+     * @return the closest integer to zero
+     */
+    public int findTheClosestIntegerToZero(List<Integer> list) throws EmptyListException{
         int closest = Integer.MAX_VALUE;
+        if (list.isEmpty()) throw new EmptyListException("The list is empty");
 
         for (int number : list) {
             if (number > 0 && number <= Math.abs(closest)) {
