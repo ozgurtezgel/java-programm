f22fdde50e8f26aec555ff9b8e2e0974ce6d5dca
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-03-23 11:33:38 +0100

    Exception and formatting corrected

--- src/main/java/com/company/ClosestIntegerToZeroFinder.java
+++ src/main/java/com/company/ClosestIntegerToZeroFinder.java
@@ -5,29 +5,28 @@ import java.util.List;
 public class ClosestIntegerToZeroFinder {
 
     /**
-     * The method finds the closest integer to zero. If there is a tie, the positive value is chosen.
-     * If the list ist empty, zero is returned.
-     * @param list list of integers
-     * @return the closest integer to zero
+     * The method finds the closest number to zero. If there is a tie, the positive value is chosen.
+     * If the numbers ist empty, zero is returned.
+     * @param numbers list of numbers
+     * @return the closest number to zero
      */
-    public int findTheClosestIntegerToZero(List<Integer> list) throws EmptyListException{
+    public int findTheClosestIntegerToZero(List<Integer> numbers) throws IllegalArgumentException {
         int closest = Integer.MAX_VALUE;
-        if (list.isEmpty()) throw new EmptyListException("The list is empty");
 
-        for (int number : list) {
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("The list is empty");
+        }
+
+        for (int number : numbers) {
             if (number > 0 && number <= Math.abs(closest)) {
                 closest = number;
-            }
-            else if (number < 0 && (-number) < Math.abs(closest)) {
+            } else if (number < 0 && (-number) < Math.abs(closest)) {
                 closest = number;
-            }
-            else if (number == 0) {
+            } else if (number == 0) {
                 closest = number;
                 break;
             }
         }
         return closest;
     }
-
-
 }
