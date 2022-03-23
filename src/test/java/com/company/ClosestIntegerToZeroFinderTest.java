761d34ed156c295215cc4d5b84983ce43af5b1cc
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-03-23 11:40:10 +0100

    Test class corrected according to conventions

--- src/test/java/com/company/ClosestIntegerToZeroFinderTest.java
+++ src/test/java/com/company/ClosestIntegerToZeroFinderTest.java
@@ -15,38 +15,43 @@ class ClosestIntegerToZeroFinderTest {
     void shouldFindTheClosestNumber() {
         ClosestIntegerToZeroFinder closestIntegerToZeroFinder = new ClosestIntegerToZeroFinder();
         List<Integer> integerList = List.of(7,5,2,-1,2,-5,7);
+
         assertEquals(-1, closestIntegerToZeroFinder.findTheClosestIntegerToZero(integerList));
     }
 
     @Test
     @DisplayName("Test with two numbers with same value, one positive one negative, expected ok")
-    void testWithTwoNumbersWithSameValueOnePositiveOneNegative(){
+    void testWithTwoNumbersWithSameValueOnePositiveOneNegative() {
         ClosestIntegerToZeroFinder closestIntegerToZeroFinder = new ClosestIntegerToZeroFinder();
         List<Integer> firstIntegerList = List.of(2,-1,2,1);
+
         assertEquals(1, closestIntegerToZeroFinder.findTheClosestIntegerToZero(firstIntegerList));
     }
 
     @Test
     @DisplayName("Test with two number with the same absolute, expected ok")
-    void testWithTwoNumbersWithSameAbsoluteValue(){
+    void testWithTwoNumbersWithSameAbsoluteValue() {
         ClosestIntegerToZeroFinder closestIntegerToZeroFinder = new ClosestIntegerToZeroFinder();
         List<Integer> integerList = List.of(11,-8,4,-4,7);
+
         assertEquals(4, closestIntegerToZeroFinder.findTheClosestIntegerToZero(integerList));
     }
 
     @Test
     @DisplayName("Test with zero in the list, expected ok")
-    void testWithZeroInTheList(){
+    void testWithZeroInTheList() {
         ClosestIntegerToZeroFinder closestIntegerToZeroFinder = new ClosestIntegerToZeroFinder();
         List<Integer> integerList = List.of(11,-8,4,0);
+
         assertEquals(0,closestIntegerToZeroFinder.findTheClosestIntegerToZero(integerList));
     }
 
     @Test
     @DisplayName("Test with an empty list, expected ok")
-    void shouldThrowExceptionWithAnEmptyList(){
+    void shouldThrowExceptionWithAnEmptyList() {
         ClosestIntegerToZeroFinder closestIntegerToZeroFinder = new ClosestIntegerToZeroFinder();
         List<Integer> integerList = new ArrayList<>();
+
         assertThrows(IllegalArgumentException.class, () -> {closestIntegerToZeroFinder.findTheClosestIntegerToZero(integerList);},
                 "IllegalArgumentException was expected");
     }
